4326b11d86e3dd90e3e77cfae5ddb058324bbc4a
SPENCER <sheldon.spencer@gmail.com>
2021-04-05 19:25:14 -0400

    update

--- src/fetchservice/fetchservice.jsx
+++ src/fetchservice/fetchservice.jsx
@@ -1,6 +1,7 @@
 import { createApolloFetch } from "apollo-fetch";
 const fetch = createApolloFetch({
   uri: "http://localhost:5000/graphql",
+  // uri: "https://heroku-seller-app.herokuapp.com/graphql",
   mode: "cors", // no-cors, *cors, same-origin
 });
 
