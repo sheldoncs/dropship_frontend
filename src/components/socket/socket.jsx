679e6baf6b6ccb8ab51fd256bccbec375584dd54
SPENCER <sheldon.spencer@gmail.com>
2021-04-05 20:57:51 -0400

    node version update

--- src/components/socket/socket.jsx
+++ src/components/socket/socket.jsx
@@ -1,6 +1,6 @@
 import socketClient from "socket.io-client";
 //
-const socket = socketClient("http://localhost:5000/");
+const socket = socketClient("https://heroku-seller-app.herokuapp.com");
 socket.on("connection", () => {
   console.log(`I'm connected with the back-end`);
 });
